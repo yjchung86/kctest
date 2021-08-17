cfd52b057cd0307d34da039f1c00f3d22ac74124
Young-Jin Chung <young-jin.chung@bc.gov.ca>
2021-08-17 13:57:26 -0700

    Added pages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Home</h2>
     </div>
   );
-}
+};
+
+const About = () => {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+};
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+};
 
 export default App;
